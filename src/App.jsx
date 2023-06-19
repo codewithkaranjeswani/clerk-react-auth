7ff8426086b65ea36837625607e1a1149bb17dc8
Karan Jeswani <krnjeswani21@gmail.com>
2023-06-20 00:42:21 +0530

    using clerk for auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,85 @@
+import {
+  ClerkProvider,
+  SignedIn,
+  SignedOut,
+  RedirectToSignIn,
+  SignIn,
+  SignUp,
+  UserButton,
+} from "@clerk/clerk-react";
+import { Link, BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
 import "./index.css"
+
+if (!import.meta.env.VITE_REACT_APP_CLERK_PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
+
+const clerkPubKey = import.meta.env.VITE_REACT_APP_CLERK_PUBLISHABLE_KEY;
+
+function PublicPage() {
+  return (
+    <div className="flex flex-col items-center">
+      <h1 className="text-3xl font-sans my-10">Public page</h1>
+      <Link className="border-2" to="/protected">Go to protected page</Link>
+    </div>
+  );
+}
+
+function ProtectedPage() {
+  return (
+    <div className="flex flex-col items-center my-10">
+      <h1 className="text-3xl font-sans mb-10">Protected page</h1>
+      <UserButton />
+    </div>
+  );
+}
+
+function ClerkProviderWithRoutes() {
+  const navigate = useNavigate();
+
+  return (
+    <ClerkProvider
+      publishableKey={clerkPubKey}
+      navigate={(to) => navigate(to)}
+    >
+      <Routes>
+        <Route path="/" element={<PublicPage />} />
+        <Route
+          path="/sign-in/*"
+          element={
+            <SignIn appearance={{ elements: { rootBox: "mx-auto my-10" } }} routing="path" path="/sign-in" />
+          }
+        />
+        <Route
+          path="/sign-up/*"
+          element={
+            <SignUp appearance={{ elements: { rootBox: "mx-auto my-10" } }} routing="path" path="/sign-up" />
+          }
+        />
+        <Route
+          path="/protected"
+          element={
+            <>
+              <SignedIn>
+                <ProtectedPage />
+              </SignedIn>
+              <SignedOut>
+                <RedirectToSignIn />
+              </SignedOut>
+            </>
+          }
+        />
+      </Routes>
+    </ClerkProvider>
+  );
+}
 
 function App() {
   return (
-    <>
-      <div className="bg-black border-2 border-blue-700">
-        <h3 className="bg-slate-400">Hello World!</h3>
-      </div>
-    </>
-  )
+    <BrowserRouter>
+      <ClerkProviderWithRoutes />
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
